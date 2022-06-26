7f2ef85f678aae3bef039b47bbf77330193daf15
Zhang-Zexi <2445459509@qq.com>
2022-06-26 16:54:01 +0800

    feat: 添加不可变集合

---
+++ src/main/java/com/zzx/api/CollectionTest.java
@@ -0,0 +1,32 @@
+package main.java.com.zzx.api;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Java11 集合增强
+ */
+public class CollectionTest {
+
+    public static void main(String[] args) {
+        var list = List.of("Java", "Python", "C"); //不可变集合
+        var copy = List.copyOf(list); //copyOf判断是否是不可变集合类型，如果是直接返回
+        System.out.println(list == copy); // true
+
+        var list2 = new ArrayList<String>(); // 这里返回正常的集合
+        var copy2 = List.copyOf(list2); // 这里返回一个不可变集合
+        System.out.println(list2 == copy2); // false
+
+
+        var set = Set.of("Java", "Python", "C");
+        var copy3 = Set.copyOf(set);
+        System.out.println(set == copy3); // true
+
+        var set1 = new HashSet<String>();
+        var copy4 = List.copyOf(set1);
+        System.out.println(set1 == copy4); // false
+
+    }
+}
